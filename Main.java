e63ce9d1682a6d3a375c6dd3756dc9eafe0bca88
omerfaruksanli <119735918+omerfaruksanli@users.noreply.github.com>
2023-01-08 15:49:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            int isPrime = 1;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+            if (isPrime == 1) {
+                System.out.print(i + " . ");
+            }
+
+        }
+
+
+
+    }
+
+}
